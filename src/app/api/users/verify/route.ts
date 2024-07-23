165af9cf4b8667063a38f616e3f4e88bd7a3361e
Tilak Singh <tsingh99991@gmail.com>
2024-07-23 17:39:17 +0530

    verified route updated

---
+++ src/app/api/users/verify/route.ts
@@ -0,0 +1,51 @@
+import { connect } from "@/db/dbConfig";
+import User from "@/models/userModel";
+import { NextResponse, NextRequest } from "next/server";
+
+//necessary to connect with data base
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { token } = reqBody;
+    console.log(token);
+
+    const user = await User.findOne(
+      { verifyToken: token },
+      {
+        verifyTokenExpiry: { $gt: Date.now() },
+      }
+    );
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          error: "invalid user token",
+        },
+        { status: 400 }
+      );
+    }
+
+    console.log(user);
+    //clean up
+    user.isVerified = true;
+    user.verifyToken = undefined;
+    user.verifyTokenExpiry = undefined;
+    await user.save();
+    return NextResponse.json(
+      {
+        message: "user verified successfully",
+        success: true,
+      },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        error: error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
