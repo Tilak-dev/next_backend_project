27c286c7204fde2ad2bd0f1851927eacfb5f1a09
Tilak Singh <tsingh99991@gmail.com>
2024-07-25 17:31:26 +0530

    complete nextjs backend with middleware

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 function page() {
-  return (
-    <div>User profile page</div>
-  )
+  const router = useRouter();
+  const [data, setData] = useState("nothing");
+
+  //functions
+  const getUserData = async () => {
+    try {
+      const res = await axios.get("/api/users/me");
+      console.log(res.data.data._id);
+      setData(res.data.data._id);
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+  const logout = async () => {
+    try {
+      const res = await axios.get("/api/users/logout")
+      toast.success("logout success fully")
+      console.log(res)
+      router.push("/login")
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+  return <div className=" flex flex-col items-center justify-center min-h-screen py-2">
+    <h1>Profile Page</h1>
+    <hr />
+    <h2>
+      {data==="nothing"? "Nothing": <Link href={`/profile/${data}`}>
+      visit to ur own Prfile {data}
+      </Link>}
+      </h2>
+      <hr />
+      <button className=" bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={logout}>Logout</button>
+      <button className=" bg-green-500 mt-4 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={getUserData}>Get user Details</button>
+  </div>;
 }
 
-export default page
+export default page;
